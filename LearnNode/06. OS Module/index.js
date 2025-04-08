30e7a2154f744066dedf9edbf9a74d679ad5990b
Sadhvi kesarwani <Sadhvikesarwani1@gmail.com>
2025-04-08 16:55:10 +0530

    update os module

---
+++ LearnNode/06. OS Module/index.js
@@ -0,0 +1,15 @@
+import os from "os";
+
+
+// console.log(os.platform());  // Get the platform of the operating system
+
+// console.log(os.arch());  // Get the architecture of the operating system
+// console.log(os.cpus());  // Get the CPU information of the operating system 
+
+// console.log(os.homedir());  // Get the home directory of the operating system   
+
+// console.log(os.networkInterfaces()); // Get the network interfaces of the operating system
+
+// console.log(os.freemem()); // Get the free memory of the operating 
+
+console.log(os.totalmem()); // Get the total memory of the operating system
